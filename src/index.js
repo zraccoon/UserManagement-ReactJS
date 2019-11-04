3d46c04574134e90cda471f622382342e8557a52
Yuri0223 <bluegem223@gmail.com>
2019-11-05 00:19:30 +1000

    import css

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "mdbreact/dist/css/mdb.css";
+import "./css/myCSS.css";
 
 
 ReactDOM.render((
